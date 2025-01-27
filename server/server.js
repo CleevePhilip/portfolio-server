b591d8f1d30aa8fe4141fbac48dc21c26e2ed0f1
CleevePhilip <147903474+CleevePhilip@users.noreply.github.com>
2025-01-27 11:27:02 +0800

    Initial express setup

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const PORT = 8000;
+const app = express();
+
+app.get("/api/test", async (req, res) => {
+  try {
+    res.json({ message: "success" });
+  } catch (error) {
+    console.log("error", error);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
