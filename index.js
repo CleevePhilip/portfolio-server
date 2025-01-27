0a02e27faf0113f6ea5b0b633d778f787b5a76cb
CleevePhilip <147903474+CleevePhilip@users.noreply.github.com>
2025-01-27 12:02:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const data = [
+  {
+    id: 1,
+    name: "test",
+  },
+  {
+    id: 2,
+    name: "test",
+  },
+  {
+    id: 3,
+    name: "test",
+  },
+];
 
 app.get("/", (req, res) => {
-  res.send("Test");
+  res.send(data);
 });
 
 app.listen(port, () => {
