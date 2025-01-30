cffd04f0e2f656736eddbf225c57c577ea22998b
CleevePhilip <147903474+CleevePhilip@users.noreply.github.com>
2025-01-30 11:10:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const nodemailer = require("nodemailer");
-const port = 8000;
+const port = process.env.PORT;
 const data = "hello";
 const test = "test";
+
 app.get("/", async (req, res) => {
   try {
     res.send(data);
@@ -20,6 +21,48 @@ app.get("/test", async (req, res) => {
     console.log(data);
   } catch (error) {
     console.log(error);
+  }
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.VERCEL_BASED_URL,
+    credentials: true,
+  })
+);
+
+app.post("/send_mail/:email/:name/:message", async (req, res) => {
+  try {
+    const { email, message, name } = req.params;
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp-relay.brevo.com",
+      port: 587,
+      auth: {
+        user: process.env.SMTP_USER, // Store this in .env
+        pass: process.env.PASSWORD_LOGIN,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.ORIGIN_EMAIL,
+      to: process.env.SENDTO_EMAIL,
+      subject: "PORTFOLIO CONTACT ME",
+      text: `EMAIL: ${email}\nNAME: ${name}\nMESSAGE: ${message}`,
+    };
+
+    // Send the email using async/await
+    const info = await transporter.sendMail(mailOptions);
+
+    console.log("Email sent:", info.response);
+    res
+      .status(200)
+      .json({ success: true, message: "Email sent successfully!" });
+  } catch (error) {
+    console.error("Error sending email:", error.message);
+    res.status(500).json({ error: "Failed to send email" });
   }
 });
 
