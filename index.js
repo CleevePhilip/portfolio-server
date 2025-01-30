fa7e743c36415e025ec658615928bb530c8e6b68
CleevePhilip <147903474+CleevePhilip@users.noreply.github.com>
2025-01-30 10:58:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,6 @@ require("dotenv").config();
 const nodemailer = require("nodemailer");
 const port = 8000;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(
-  cors({
-    origin: process.env.VERCEL_BASED_URL,
-    credentials: true,
-  })
-);
-
-const data = [
-  { id: 1, name: "test1" },
-  { id: 2, name: "test2" },
-  { id: 3, name: "test3" },
-];
 app.get("/", async () => {
   try {
     res.json({ data });
