02e713c3466337dd9a7e829991e6ce93e453f616
CleevePhilip <147903474+CleevePhilip@users.noreply.github.com>
2025-01-30 10:55:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,38 +14,6 @@ app.use(
   })
 );
 
-app.post("/send_mail/:email/:name/:message", async (req, res) => {
-  try {
-    const { email, message, name } = req.params;
-
-    const transporter = nodemailer.createTransport({
-      host: "smtp-relay.brevo.com",
-      port: 587,
-      auth: {
-        user: process.env.SMTP_USER, // Store this in .env
-        pass: process.env.PASSWORD_LOGIN,
-      },
-    });
-
-    const mailOptions = {
-      from: process.env.ORIGIN_EMAIL,
-      to: process.env.SENDTO_EMAIL,
-      subject: "PORTFOLIO CONTACT ME",
-      text: `EMAIL: ${email}\nNAME: ${name}\nMESSAGE: ${message}`,
-    };
-
-    // Send the email using async/await
-    const info = await transporter.sendMail(mailOptions);
-
-    console.log("Email sent:", info.response);
-    res
-      .status(200)
-      .json({ success: true, message: "Email sent successfully!" });
-  } catch (error) {
-    console.error("Error sending email:", error.message);
-    res.status(500).json({ error: "Failed to send email" });
-  }
-});
 const data = [
   { id: 1, name: "test1" },
   { id: 2, name: "test2" },
